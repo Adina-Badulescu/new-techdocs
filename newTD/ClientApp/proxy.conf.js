bc22c3fb0a85491c6e7ea87cbbeb22419444b239
Adrian Badulescu <adrian.badulescu@nn.ro>
2023-02-22 12:22:54 +0200

    list and gettemplate endpoints added

--- newTD/ClientApp/proxy.conf.js
+++ newTD/ClientApp/proxy.conf.js
@@ -7,6 +7,7 @@ const PROXY_CONFIG = [
   {
     context: [
       "/getdomain",
+      "/templates"
    ],
     target: target,
     secure: false,
--- newTD/Controllers/GetTemplatesController.cs
+++ newTD/Controllers/GetTemplatesController.cs
@@ -1,26 +1,41 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using newTD.Entities;
 using Whois;
 
 namespace newTD.Controllers
 {
     [ApiController]
     [Route("[controller]")]    
-    public class GetTemplatesController : ControllerBase
+    public class TemplatesController : ControllerBase
     {
-        private readonly ILogger<GetTemplatesController> _logger;
-        public GetTemplatesController(ILogger<GetTemplatesController> logger)
+        private readonly ILogger<TemplatesController> _logger;
+        public TemplatesController(ILogger<TemplatesController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet("list")]
-        public async Task<IActionResult> ListTemplates([FromRoute] string list)
+        [HttpGet("List")]
+        public  IActionResult ListTemplates()
+        {
+            try
+            {                             
+                return Ok("hello");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("error into Query() " + ex.Message);
+                throw;
+            }
+
+        }
+
+        [HttpGet("GetId")]
+        public IActionResult GetTemplate([FromQuery] string Id)
         {
             try
             {
-                
-                return Ok(list);
+                return Ok(Id);
             }
             catch (Exception ex)
             {
